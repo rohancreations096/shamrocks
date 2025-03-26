3fce27969ed02359def80fa407a7f44bb5e82a7d
rohancreations096 <pavanrohanbunga18@gmail.com>
2025-03-26 22:27:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ model = joblib.load('loan_data1.joblib')
 gender=st.number_input('enter gender ')
 married=st.number_input('enter martial status ')
 income=st.number_input('enter income ')
-ld=st.number_input('enter loan amount')
+la=st.number_input('enter loan amount')
 
 if st.button('predict approval'):
-    prediction=model.predict([[gender,married,income,ld]])
+    prediction=model.predict([[gender,married,income,la]])
     if prediction == 'Y':
         st.text('loan approved')
     else:
